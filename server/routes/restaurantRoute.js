29973fa56048a6ec66f0d82f89db72df66bc741d
Srivarshan <srivarshan611@gmail.com>
2022-07-18 15:58:03 +0530

    added routes

---
+++ server/routes/restaurantRoute.js
@@ -0,0 +1,105 @@
+const router = require('express').Router();
+const db = require('../db');
+
+// Get all Restaurents
+router.get('/getRestaurants', async (req, res) => {
+  try {
+    const results = await db.query('SELECT * FROM restaurants');
+
+    // const restaurantRatingsData = await db.query(
+    //   'select * from restaurants left join (select restaurant_id, COUNT(*), TRUNC(AVG(rating),1) as average_rating from reviews group by restaurant_id) reviews on restaurants.id = reviews.restaurant_id;'
+    // );
+
+    res.status(200).json({
+      status: 'success',
+      result: results.rows.length,
+      data: {
+        restaurants: results.rows,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// Get specific Restaurant
+router.get('/getRestaurant/:id', async (req, res) => {
+  try {
+    // const restaurant = await db.query(
+    //   'select * from restaurants left join (select restaurant_id, COUNT(*), TRUNC(AVG(rating),1) as average_rating from reviews group by restaurant_id) reviews on restaurants.id = reviews.restaurant_id where id = $1',
+    //   [req.params.id]
+    // );
+    const restaurant = await db.query(
+      `select * from restaurants where id = ${req.params.id}`
+    );
+
+    //  const reviews = await db.query(
+    //    'select * from reviews where restaurant_id = $1',
+    //    [req.params.id]
+    //  );
+
+    res.status(200).json({
+      status: 'succes',
+      data: {
+        restaurant: restaurant.rows[0],
+        // reviews: reviews.rows,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// Create Restaurent
+router.post('/createRestaurant', async (req, res) => {
+  try {
+    const results = await db.query(
+      'INSERT INTO restaurants (name, location, price_range) values ($1, $2, $3) returning *',
+      [req.body.name, req.body.location, req.body.price_range]
+    );
+
+    res.status(201).json({
+      status: 'succes',
+      data: {
+        restaurant: results.rows[0],
+      },
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// Update Restaurent
+router.put('/updateRestaurant/:id', async (req, res) => {
+  try {
+    const results = await db.query(
+      'UPDATE restaurants SET name = $1, location = $2, price_range = $3 where id = $4 returning *',
+      [req.body.name, req.body.location, req.body.price_range, req.params.id]
+    );
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        retaurant: results.rows[0],
+      },
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// Delete Restaurent
+router.delete('/deleteRestaurant/:id', async (req, res) => {
+  try {
+    const results = db.query('DELETE FROM restaurants where id = $1', [
+      req.params.id,
+    ]);
+    res.status(204).json({
+      status: 'success',
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,16 +1,16 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const resRoute = require('./routes/restaurentRoute');
-// const db = require('./db');
+const resRoute = require('./routes/restaurantRoute');
 const morgan = require('morgan');
 
 const app = express();
+app.use(morgan('dev'));
 const port = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/v1/restaurents', resRoute);
+app.use('/api/v1/restaurants', resRoute);
 
 app.listen(port, () => console.log('listening on port ' + port));
